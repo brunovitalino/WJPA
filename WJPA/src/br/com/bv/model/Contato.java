af9beee69215b0e84435169116edd469fcd200d9
Bruno <Bruno@BrunoNB.localdomain>
2017-04-17 18:45:07 -0300

    Atualizado nosso POJO. Creada e implementada a classe de adicionar um novo contato.
    Signed-off-by: Bruno <Bruno@BrunoNB.localdomain>

--- WJPA/src/br/com/bv/model/Contato.java
+++ WJPA/src/br/com/bv/model/Contato.java
@@ -2,6 +2,7 @@ package br.com.bv.model;
 
 import java.util.Calendar;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,16 +13,29 @@ import javax.persistence.TemporalType;
 
 @Entity
 //@Table(name="contatos")
+//@NamedQuery(name = "todosContatos", query = "SELECT nome FROM Organization nome")
 public class Contato
 {
-	@Id	
+	@Column(length=6)
+	@Id
 	@GeneratedValue
 	private int id;
+	
+	@Column(length=25, nullable=false)
 	private String nome;
+
+	@Column(length=20)
 	private String telefone;
+	
+	@Column(length=50)
+	private String email;
+
+	@Column(name="dataMensagem")
 	@Temporal(TemporalType.DATE)
-	//@Column(name="dataEntrada", nullable=true)
 	private Calendar data;
+	
+	@Column(length=500)
+	private String mensagem;
 	
 	public int getId() {
 		return id;
@@ -41,11 +55,23 @@ public class Contato
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public Calendar getData() {
 		return data;
 	}
 	public void setData(Calendar data) {
 		this.data = data;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
 	}
 
 }
